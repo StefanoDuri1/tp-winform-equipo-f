7182aef8c22d05a2e63ad1ab421a1d64cb3abf62
StefanoDuri1 <duristefano@gmail.com>
2024-12-21 03:53:57 -0300

    creo clase articulo y catalogo

---
+++ comercio/Articulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comercio
+{
+    internal class Articulo
+    {
+        public int CodArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public string UrlImagen { get; set; }
+        public float Precio { get; set; }
+    }
+}
